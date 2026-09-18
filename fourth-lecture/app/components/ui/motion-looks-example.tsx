b368eb5b84f8d4e8e8965a91a49fa053cc44ac31
ashishxdev <ashishrana2004@gmail.com>
2026-09-18 17:03:08 +0530

    fourth-lecture

--- fourth-lecture/app/components/ui/motion-looks-example.tsx
+++ fourth-lecture/app/components/ui/motion-looks-example.tsx
@@ -1,32 +1,107 @@
 "use client";
+// prettierrc - automatically sorts classes after saving file
+
 import { IconRocket } from "@tabler/icons-react";
+import {
+  useMotionValueEvent,
+  useScroll,
+  motion,
+  useTransform,
+  useMotionTemplate,
+  useSpring,
+} from "framer-motion";
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export function MotionHooksExample() {
-  // prettierrc - automatically sorts classes after saving file
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end start"],
+  });
+
+  const backgrounds = ["#343434", "#00193b", "#05291c"];
+
+  const [background, setBackground] = useState(backgrounds[0]);
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    const finalValue = Math.floor(latest * backgrounds.length);
+    // 0.44 --> 0.44 * 3 = 1.32 --> 1
+    // 0.78 --> 0.78 * 3 = 2.34 --> 2
+    // 0.01 --> 0.01 * 3 = 0.03 --> 0
+    setBackground(backgrounds[finalValue]);
+  });
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-neutral-900">
+    <motion.div
+      ref={containerRef}
+      animate={{
+        background,
+      }}
+      className="flex min-h-screen items-center justify-center bg-neutral-900"
+    >
       <div className="mx-auto flex max-w-4xl flex-col gap-10 py-40">
         {features.map((feature, idx) => (
-          <div
-            key={feature.title}
-            className="grid grid-cols-2 items-center gap-20 py-40"
-          >
-            <div className="flex flex-col gap-5">
-              {feature.icon}
-              <h2 className="text-4xl font-bold text-white">{feature.title}</h2>
-              <p className="text-lg text-neutral-400">{feature.description}</p>
-            </div>
-
-            <div>{feature.content}</div>
-          </div>
+          <Card key={feature.title} feature={feature} />
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
+
+const Card = ({ feature }: { feature: Feature }) => {
+  // useScroll is used to create scroll-linked animations, like progress indicators and parallax effects.
+  const ref = useRef<HTMLDivElement>(null); // this is a react hook used to reference to the html element and it will tell us the position of that element when we scroll
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"], // start end means when the start of the element is at the end of the viewport, end start means when the end of the element is at the start of the viewport, top bottom means when the top of the element is at the bottom of the viewport, bottom top means when the bottom of the element is at the top of the viewport
+  });
+
+  // useMotionValueEvent manages a motion value event handler throughout the lifecycle of a React component.
+  // useMotionValueEvent(scrollYProgress, "change", (latest) => {
+  //   console.log("the latest value of scrollYProgress is", latest);
+  // });
+
+  const translateContent = useSpring(
+    useTransform(scrollYProgress, [0, 1], [-100, 200]),
+    {
+      stiffness: 100,
+      damping: 30,
+      mass: 1,
+    },
+  ); // useTransform creates a new motion value that transforms the output of one or more motion values. useSpring creates a motion value that will animate to its latest target with a spring animation this makes the animation much smoother.
+  const opacityContent = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0]);
+  const blurContent = useTransform(scrollYProgress, [0.5, 1], [0, 10]);
+  const scale = useTransform(scrollYProgress, [0.5, 1], [1, 0.8]);
+
+  return (
+    <div
+      ref={ref}
+      key={feature.title}
+      className="grid grid-cols-2 items-center gap-20 py-40"
+    >
+      <motion.div
+        style={{
+          filter: useMotionTemplate`blur(${blurContent}px)`, // we cant use motion value as string interpolation in css so for that we can use useMotionTemplate
+          scale,
+        }}
+        className="flex flex-col gap-5"
+      >
+        {feature.icon}
+        <h2 className="text-4xl font-bold text-white">{feature.title}</h2>
+        <p className="text-lg text-neutral-400">{feature.description}</p>
+      </motion.div>
+      <motion.div
+        style={{
+          y: translateContent,
+          opacity: opacityContent,
+        }}
+      >
+        {feature.content}
+      </motion.div>
+    </div>
+  );
+};
 
 type Feature = {
   icon: React.ReactNode;
